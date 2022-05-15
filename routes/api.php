1b6288cfa06aa85f0cb9069ac8f62bd0885c9b24
maucaruso <carusojr@outlook.com>
2022-05-15 08:56:29 -0300

    RealState model and controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->namespace('App\\Http\\Controllers\\Api\\')->group(function () {
+
+    Route::prefix('real-states')->name('real-states.')->group(function () {
+
+        Route::get('/', 'RealStateController@index')->name('index');
+    });
+});
